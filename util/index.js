7954612c46f938d805104c14c8185774ef477121
Ante Botic <121798405+forgetful-functor@users.noreply.github.com>
2023-02-28 11:20:28 +0100

    accounts: fields, signing & verifying

--- util/index.js
+++ util/index.js
@@ -1,4 +1,7 @@
 const keccak256 = require("js-sha3").keccak256
+const EC = require('elliptic').ec
+
+const ec = new EC('secp256k1')
 
 const sortCharacters = data => {
     return JSON.stringify(data).split('').sort().join('')
@@ -10,9 +13,10 @@ const keccakHash = data => {
     hash.update(sortCharacters(data))
 
     return hash.hex()
-} 
+}
 
 module.exports = {
     sortCharacters,
-    keccakHash
+    keccakHash,
+    ec
 }
